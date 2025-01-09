71b360acc75d9c1796dffa63db8a6d5fedd2cb70
ryuseunghan <jorzor99@gmail.com>
2025-01-09 11:35:22 +0900

    Fix : exlint 수정
    eslint 관련 설정 변경
    
    RelatedTo : none

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,15 @@ export default [
 			ecmaVersion: 2021,
 			sourceType: "module",
 		},
-		rules: {
-			"no-console": "warn",
-		},
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "eqeqeq": ["error", "always"],
+      "curly": ["error", "all"],
+      "default-case": "warn",
+      "prettier/prettier": "error",
+      "no-undef": "error",
+      "no-redeclare": "error",
+    },
 	},
 ];
